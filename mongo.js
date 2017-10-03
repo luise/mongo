ad53b75a57cd2925f3475c2d808b83f76d0e4dda
Kay Ousterhout <kay@quilt.io>
2017-10-03 12:19:22 -0700

    blueprint: Rename cluster to containers
    containers is more explicit and more consistent with other
    blueprints.

--- mongo.js
+++ mongo.js
@@ -7,34 +7,34 @@ function getHostname(c) {
 }
 
 function Mongo(nWorker) {
-  this.cluster = [];
+  this.containers = [];
   for (let i = 0; i < nWorker; i += 1) {
-    this.cluster.push(new Container('mongo', image));
+    this.containers.push(new Container('mongo', image));
   }
 
-  const hostnames = this.cluster.map(getHostname).join(',');
-  this.cluster.forEach((m) => {
+  const hostnames = this.containers.map(getHostname).join(',');
+  this.containers.forEach((m) => {
     m.setEnv('MEMBERS', hostnames);
   });
 
   // The initiator is choosen completley arbitrarily.
-  this.cluster[0].setEnv('INITIATOR', 'true');
+  this.containers[0].setEnv('INITIATOR', 'true');
 
-  allow(this.cluster, this.cluster, this.port);
+  allow(this.containers, this.containers, this.port);
 }
 
 Mongo.prototype.port = 27017;
 
 Mongo.prototype.deploy = function deploy(deployment) {
-  this.cluster.forEach(container => container.deploy(deployment));
+  this.containers.forEach(container => container.deploy(deployment));
 };
 
 Mongo.prototype.allowFrom = function allowFrom(from, p) {
-  allow(from, this.cluster, p);
+  allow(from, this.containers, p);
 };
 
 Mongo.prototype.uri = function uri(dbName) {
-  return `mongodb://${this.cluster.map(getHostname).join(',')}/${dbName}`;
+  return `mongodb://${this.containers.map(getHostname).join(',')}/${dbName}`;
 };
 
 module.exports = Mongo;
